0b83ccd3a0486034568ee1247cff0538a235de81
Nathaniel Saul <nat@saulgill.com>
2016-11-08 05:44:42 +0000

    moving icri stuff to its own setup

--- setup.sh
+++ setup.sh
@@ -2,12 +2,11 @@
 
 # copy files into root directory
 cp dotfiles/.* ~/.
-
 apt-get install vim
-apt-get install python3-pip
-
-
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+vim +PluginInstall +qall
 
-
+git clone https://github.com/sauln/ICRI
+chmod u+x ICRI/setup.sh
+./ICRI/setup.sh
